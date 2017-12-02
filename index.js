7a87cabe29ef5d11f39b54e1a60e85487d3c3ab0
Chris Richardson <cjr@hotmail.co.uk>
2017-12-02 15:38:41 +0000

    add syntax for external file links

--- index.js
+++ index.js
@@ -22,6 +22,8 @@ define(module, function(exports, require) {
     file_list: [],
     excluded_paths: [],
 
+    links: {},
+
     files: {
       copy: [],
       merge: []
@@ -37,6 +39,9 @@ define(module, function(exports, require) {
           qp.each(glob.sync(asset.target), file => {
             this.add_file({ type: asset.type, file: file });
           });
+        } else if (asset.link) {
+          if (!this.links[asset.ext]) this.links[asset.ext] = [];
+          this.links[asset.ext].push(asset.target);
         }
       });
     },
@@ -48,18 +53,23 @@ define(module, function(exports, require) {
           line = qp.trim(line);
           if (qp.empty(line) || qp.starts(line, '//')) return;
           line = qp.format(line, this.state);
-          var parts = qp.map(line.split(':'), part => qp.trim(part));
-          if (parts[0] === 'asset') {
-            var child_assets = this.parse(parts[1]);
+          var parts = qp.map(line.split(/:(.+)/), part => qp.trim(part));
+          var key = parts[0];
+          var value = parts[1];
+
+          if (key === 'asset') {
+            var child_assets = this.parse(value);
             if (qp.not_empty(child_assets)) {
               qp.push(this.assets, child_assets);
             }
-          } else if (parts[0] === 'state') {
-            var kvp = qp.map(parts[1].split('='), part => qp.trim(part));
+          } else if (key === 'state') {
+            var kvp = qp.map(value.split('='), part => qp.trim(part));
             this.state[kvp[0]] = kvp[1];
+          } else if (key.slice(0, 5) === 'link_') {
+            qp.push(this.assets, { type: 'link', target: value, link: true, ext: key.slice(5) });
           } else {
-            var asset = { type: parts[0], target: this.add_path(parts[1]) };
-            asset[parts[0]] = true;
+            var asset = { type: key, target: this.add_path(value) };
+            asset[key] = true;
             qp.push(this.assets, asset);
           }
         });
